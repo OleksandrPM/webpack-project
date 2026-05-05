8aeea8279d317bdc03c34d0de499328a9ef7406e
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-05-05 15:35:23 +0200

    Update publicPath

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: '/webpack-project/',
+    publicPath: './',
     filename: 'js/[name].[contenthash].bundle.js',
     clean: true,
   },
